baa338ff904a210609d32661de9bf34d1161e24a
zhouliang <1415280714@qq.com>
2021-12-21 15:05:05 +0800

    提交测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
-},
+  rules: {}
+}
