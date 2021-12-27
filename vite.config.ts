f730a099a12516232158fd358f89c9520342bf47
zhouliang <1415280714@qq.com>
2021-12-27 13:50:03 +0800

    feat: 定制化commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,15 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve:{
-    alias:{
-      '@':resolve(__dirname,'src')
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
     }
   },
-  base:'./',
-  server:{
-    port:4000,
-    open:true,
-    cors:true
+  base: './',
+  server: {
+    port: 4000,
+    open: true,
+    cors: true
   }
 })
